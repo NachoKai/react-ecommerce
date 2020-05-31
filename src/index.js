98a2e0ebd4ad52ba065902dda1ad04c46c96bc00
Nacho Caiafa <nachokai@outlook.com>
2020-05-31 00:24:04 -0300

    add base route

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/react-ecommerce'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
